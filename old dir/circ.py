59a9a25f771dc0180d2efc0cffd4572322e99634
KachokChort <timajambam@gmail.com>
2025-02-28 17:17:07 +0300

    Delete old dir directory

--- old dir/circ.py
+++
@@ -1,9 +0,0 @@
-a = [int(i) for i in input().split()]
-b = [int(i) for i in input().split()]
-
-s = ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
-print(s)
-if s <= a[2] + b[2]:
-    print('YES')
-else:
-    print('NO')
